f77fbdb33213eee45e93939365c84f2310dbec60
Cristian Camilo Romero <cristian.romero@ficohsa.com>
2026-01-11 09:38:04 -0500

    Se ajustan nombres

--- app_hexagonal.py
+++ app_hexagonal.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from hexagonal.infrastructure.hexagonal_configurator import HexagonalConfigurator
+from agenda_assistant.infrastructure.hexagonal_configurator import HexagonalConfigurator
 
 
 def main():
